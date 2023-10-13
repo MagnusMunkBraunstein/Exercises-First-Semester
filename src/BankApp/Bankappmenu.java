1f6794152d5fb04365dbcbdcd1bdaeeccf3deda5
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2023-10-13 12:45:54 +0200

    Alle øvelser pt.

---
+++ src/BankApp/Bankappmenu.java
@@ -0,0 +1,28 @@
+package BankApp;
+
+// ikke færdig, men fungere med if
+
+import java.util.Scanner;
+
+public class Bankappmenu {
+    public static void main(String[] args) {
+        Scanner Indtast = new Scanner(System.in);
+
+        while(true){
+
+            if (Indtast.nextInt() == 1){
+            }
+
+            if (Indtast.nextInt() == 2){
+            }
+
+            if (Indtast.nextInt() == 3){
+            }
+
+
+        }
+
+
+    }
+}
+
