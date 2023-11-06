b489c7668e357b122a857dbbf8bfe90410d2d69c
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2023-11-06 13:15:17 +0100

    Alle øvelser pt.

--- src/RegnvejrToDIMArray/Rainfall.java
+++ src/RegnvejrToDIMArray/Rainfall.java
@@ -1,9 +1,31 @@
 package RegnvejrToDIMArray;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Rainfall {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int[][] data = new int [11][12];
 
+        FileReader fil = new FileReader("RainfallData.txt");
+        BufferedReader ind = new BufferedReader(fil);
+        String linje = ind.readLine();
+        int r = 0;
+        while (linje != null)
 
+
+        {
+            String[] bidder = linje.split(" ");
+            for (int c = 0; c<=11; c++) {
+                data[r][c]=Integer.parseInt(bidder[c]);
+            }
+
+            linje = ind.readLine();
+            r++;
+        }
+        fil.close();
+
+        data[2][3]=10000;
     }
 }
