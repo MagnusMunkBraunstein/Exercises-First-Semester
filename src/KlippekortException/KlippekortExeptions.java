b235bedb80db25da3306aa3f0fd442bd440b29a9
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2023-11-13 12:39:49 +0100

    Alle øvelser pt.

---
+++ src/KlippekortException/KlippekortExeptions.java
@@ -0,0 +1,4 @@
+package KlippekortException;
+
+public class KlippekortExeptions {
+}
--- src/RegnvejrToDIMArray/Rainfall.java
+++ src/RegnvejrToDIMArray/Rainfall.java
@@ -1,31 +1,50 @@
 package RegnvejrToDIMArray;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Rainfall {
+    static final int MAPALANA=0;
+    static final int RATNAPURA=1;
+    static final int SITA_ELIYA=2;
+    static final int BADULLA=3;
+    static final int MATALE=4;
+    static final int ANURADHAPURA=5;
+    static final int AMPARA=6;
+    static final int HAMBEGAMUWA=7;
+    static final int AMBALANTOTA=8;
+    static final int KIRAMA=9;
+    static final int A_PALESSA=10;
+
+    enum Month {JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+
+
     public static void main(String[] args) throws IOException {
-        int[][] data = new int [11][12];
+        int[][] data=new int[11][12];
 
         FileReader fil = new FileReader("RainfallData.txt");
         BufferedReader ind = new BufferedReader(fil);
         String linje = ind.readLine();
-        int r = 0;
+        int r=0;
         while (linje != null)
-
-
         {
             String[] bidder = linje.split(" ");
-            for (int c = 0; c<=11; c++) {
+            for (int c=0; c<=11; c++) {
                 data[r][c]=Integer.parseInt(bidder[c]);
             }
-
             linje = ind.readLine();
             r++;
         }
         fil.close();
 
-        data[2][3]=10000;
+        System.out.println(data[AMPARA][Month.MAY.ordinal()]);
+        for (int by=MAPALANA; by<=A_PALESSA; by++){
+            System.out.println(data[by][Month.MAY.ordinal()]);
+        }
+        for (Month m: Month.values()){
+            System.out.print(data[AMPARA][m.ordinal()]+"\t");
+        }
     }
 }
+
+
